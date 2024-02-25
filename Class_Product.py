fb965367ef5f3bd452a5039511dbe5be755c2bc4
NasirovEF <i@ef-nasirov.ru>
2024-02-25 23:30:00 +0300

    Add new file, add new metods in Product

--- Class_Product.py
+++ Class_Product.py
@@ -9,9 +9,23 @@ class Product:
 
     @classmethod
     def get_product(cls, name, description, price, quantity):
-        return [name, description, price, quantity]
+        return cls(name, description, price, quantity)
 
     @property
     def get_price(self):
-        if self.price <= 0:
-            return "Введена некорректная цена"
+        return self.price
+
+    @get_price.setter
+    def get_price(self, price):
+        if price <= 0:
+            return "Введена не корректная цена"
+        if price < self.price:
+            print("Заявленная цена ниже существующей, вы действительно хотите снизить цену")
+            user_input = input("Введите 'y' для подтверждения")
+            if user_input.lower() == "y":
+                self.price = price
+            else:
+                print("Цена сохранена прежняя")
+
+        else:
+            self.price = price
