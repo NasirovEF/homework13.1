e074ebbf37eecec1540f55875f65cfcad240d2c0
NasirovEF <i@ef-nasirov.ru>
2024-02-26 18:41:24 +0300

    rename file, made "price" private

--- src/class_product.py
+++ src/class_product.py
@@ -4,28 +4,28 @@ class Product:
     def __init__(self, name, description, price, quantity):  # Задание 2
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
     @classmethod
-    def get_product(cls, name, description, price, quantity):
-        return cls(name, description, price, quantity)
+    def get_product(cls, prod_dict):
+        return cls(**prod_dict)
 
     @property
     def get_price(self):
-        return self.price
+        return self.__price
 
     @get_price.setter
     def get_price(self, price):
         if price <= 0:
             return "Введена не корректная цена"
-        if price < self.price:
+        if price < self.__price:
             print("Заявленная цена ниже существующей, вы действительно хотите снизить цену")
             user_input = input("Введите 'y' для подтверждения")
             if user_input.lower() == "y":
-                self.price = price
+                self.__price = price
             else:
                 print("Цена сохранена прежняя")
 
         else:
-            self.price = price
+            self.__price = price
