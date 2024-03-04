0a7d43a45de7db5474acf84e3612d0b39ae8e36e
NasirovEF <i@ef-nasirov.ru>
2024-03-04 23:02:00 +0300

    add Mixin and Base class

--- src/class_product.py
+++ src/class_product.py
@@ -9,10 +9,22 @@ class Base(ABC):
     def get_price(self):
         pass
 
-class MixinInform:
-    def __repr__(self):
 
-class Product(Base):
+class MixinInform:
+    def __init__(self, *args):
+        print(repr(self))
+
+    def __repr__(self):
+        lst_values = []
+
+        for values in self.__dict__:
+            lst_values.append(str(values))
+        values_str = ", ".join(lst_values)
+
+        return f'{self.__class__.__name__}({values_str})'
+
+
+class Product(Base, MixinInform):
     """Класс товара"""
 
     def __init__(self, name, description, price, quantity):
@@ -20,6 +32,7 @@ class Product(Base):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     @classmethod
     def get_product(cls, prod_dict):
@@ -58,18 +71,18 @@ class Smartphone(Product):
     """класс Смартфоны"""
 
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
-        super().__init__(name, description, price, quantity)
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
+        super().__init__(name, description, price, quantity)
 
 
 class LawnGrass(Product):
     """класс Трава газонная"""
 
     def __init__(self, name, description, price, quantity, country, germination, color):
-        super().__init__(name, description, price, quantity)
         self.country = country
         self.germination = germination
         self.color = color
+        super().__init__(name, description, price, quantity)
